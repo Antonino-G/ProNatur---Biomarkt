8f14768168fb8e920b4472cfd7b89694cc33c238
Antonino-G <116971639+Antonino-G@users.noreply.github.com>
2023-09-02 16:28:54 +0200

    add project

---
+++ LoadingScreen.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProNatur_Biomarkt_GmbH
+{
+    public partial class LoadingScreen : Form
+    {
+        private int loadingbarValue;
+
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            loadingbarTimer.Start();
+        }
+
+        private void loadingbarTimer_Tick(object sender, EventArgs e)
+        {
+            loadingbarValue += 1;
+
+            lblLoadingProgress.Text = loadingbarValue.ToString() + "%";
+            loadingProgressbar.Value = loadingbarValue;
+
+            if(loadingbarValue >= loadingProgressbar.Maximum)
+            {
+                loadingbarTimer.Stop();
+
+                // Finish loading show main menu screen
+                MainMenuScreen mainMenuScreen = new MainMenuScreen();
+                mainMenuScreen.Show();
+
+                this.Hide();
+            }
+
+        }
+
+
+    }
+}
---
+++ MainMenuScreen.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ProNatur_Biomarkt_GmbH
+{
+    partial class MainMenuScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuScreen));
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.btnBill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnProducts.BackgroundImage")));
+            this.btnProducts.ForeColor = System.Drawing.Color.White;
+            this.btnProducts.Location = new System.Drawing.Point(16, 31);
+            this.btnProducts.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(275, 125);
+            this.btnProducts.TabIndex = 0;
+            this.btnProducts.Text = "Produkte verwalten";
+            this.btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // btnBill
+            // 
+            this.btnBill.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBill.BackgroundImage")));
+            this.btnBill.ForeColor = System.Drawing.Color.White;
+            this.btnBill.Location = new System.Drawing.Point(329, 31);
+            this.btnBill.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnBill.Name = "btnBill";
+            this.btnBill.Size = new System.Drawing.Size(275, 125);
+            this.btnBill.TabIndex = 1;
+            this.btnBill.Text = "Rechnung erstellen";
+            this.btnBill.UseVisualStyleBackColor = true;
+            // 
+            // MainMenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(620, 194);
+            this.Controls.Add(this.btnBill);
+            this.Controls.Add(this.btnProducts);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "MainMenuScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hauptmenü";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProducts;
+        private System.Windows.Forms.Button btnBill;
+    }
+}
